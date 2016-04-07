03a03cb49ef9da20c558c4b1be0df29f6c71b30f
Masakatsu ITO <itoh.masakatsu@jaxa.jp>
2016-04-07 17:36:00 +0900

    airCooledCylinder.hpp added

---
+++ src/test/airCooledCylinder.hpp
@@ -0,0 +1,134 @@
+/*
+ * airCooledCylinder.hpp
+ *
+ *  Created on: 2016/03/23
+ *      Author: Masakatsu ITO
+ */
+
+#ifndef AIRCOOLEDCYLINDER_HPP_
+#define AIRCOOLEDCYLINDER_HPP_
+
+class ExactTempDist
+{
+private:
+	const double CylinderLength, AmbientTemperature, HotTemperature;
+	const double n;
+
+public:
+	explicit ExactTempDist(double CylinderLength_,
+			double AmbientTemperature_, double HotTemperature_,
+			double ThermalConductivity, double Area,
+			double ConvectiveHeatTransCoeff, double Circumference) :
+			CylinderLength( CylinderLength_),
+			AmbientTemperature( AmbientTemperature_),
+			HotTemperature( HotTemperature_),
+			n( sqrt( ConvectiveHeatTransCoeff * Circumference /
+							( ThermalConductivity * Area ) ) )
+	{}
+
+	double operator()( double x) const {
+		return ( AmbientTemperature +
+				(HotTemperature - AmbientTemperature ) *
+				 cosh( n * ( CylinderLength - x ) ) /
+				 cosh( n * CylinderLength ) );
+	}
+};
+
+const int NumCtrlVol = 5;
+
+const double CylinderLength = 1.0;
+
+const double AmbientTemperature = 20.0,
+		HotTemperature = 100.0;
+
+const double ThermalConductivity = 1000.0;
+
+// http://www.engineeringtoolbox.com/convective-heat-transfer-d_430.html
+const double ConvectiveHeatTransCoeff = 0.7;
+
+const double CylinderRadius = 2.0 / 25.0 * ConvectiveHeatTransCoeff /
+												ThermalConductivity,
+			Area = M_PI * CylinderRadius * CylinderRadius,
+			Circumference = 2.0 * M_PI *  CylinderRadius;
+
+const double convergenceCriterion = 1.0e-7;
+
+void printConstants()
+{
+
+	std::cout << "The number of control volumes = " <<
+			NumCtrlVol << std::endl;
+
+	std::cout << "ThermalConductivity * Area = " <<
+			ThermalConductivity * Area << std::endl;
+
+	std::cout << "- ConvectiveHeatTransCoeff * Circumference = " <<
+			- ConvectiveHeatTransCoeff * Circumference << std::endl;
+
+	std::cout << "ConvectiveHeatTransCoeff * Circumference * " <<
+			"AmbientTemperature = " <<
+			ConvectiveHeatTransCoeff * Circumference * AmbientTemperature <<
+			std::endl;
+}
+
+
+template < typename MatrixType >
+void printCoefficients( const MatrixType & coeffMat)
+{
+	int ri, ci;
+
+	const double scale = - ThermalConductivity * Area;
+	std::cout << "scale = - ThermalConductivity * Area" <<
+			std::endl;
+	std::cout << "= " << scale << std::endl;
+
+	std::cout << "Coefficient matrix after applying boundary conditions" <<
+			std::endl;
+	std::cout << " / scale" << std::endl;
+	for (ri = 0; ri < coeffMat.rowSize(); ri++)
+	{
+		for (ci = 0; ci < coeffMat.columnSize(); ci++)
+			std::cout << std::setw(11) << std::fixed << std::setprecision(4) <<
+				coeffMat(ri, ci) / scale;
+		std::cout << std::endl;
+	}
+
+}
+
+template < typename VectorType >
+void printRHS( const VectorType & rhsVec)
+{
+	std::cout << std::endl;
+	std::cout << "RHS vector after applying boundary conditions" <<
+			std::endl;
+	std::cout << " / scale" << std::endl;
+	for (int ci = 0; ci < rhsVec.columnSize(); ci++)
+		std::cout << std::setw(11) << std::fixed << std::setprecision(4) <<
+						rhsVec( ci) / scale << std::endl;
+}
+
+
+template < typename VectorType >
+void printCalculatedAndExactTemperatureDistributions(
+		const VectorType & temperature)
+{
+	ExactTempDist exactDist(CylinderLength,
+				AmbientTemperature, HotTemperature,
+				ThermalConductivity, Area,
+				ConvectiveHeatTransCoeff, Circumference);
+
+	std::cout << "# stationary temperature distribution" << std::endl;
+	std::cout << "#     x      FVM       exact " << std::endl;
+
+	for (int ci =0; ci < temperature.columnSize(); ci++) {
+		double x = CylinderLength / temperature.columnSize() * (ci + 0.5);
+		std::cout << std::setw( 8) << std::fixed << std::setprecision(2) << x;
+		std::cout << std::setw(10) << std::fixed << std::setprecision(3) <<
+				temperature( ci);
+		std::cout << std::setw(10) << std::fixed << std::setprecision(3) <<
+				exactDist( x) << std::endl;
+	}
+}
+
+
+#endif /* SAIRCOOLEDCYLINDER_HPP_ */
